31a94463c04241e3d86ac72d9212b8f9d5f9c8b7
haroruhomer <harmol22@hotmail.com>
2015-11-17 01:18:20 -0500

    Feat: Checklist  funcional
    Se ha arreglado el checklist

--- app/controllers/control_estados_controller.rb
+++ app/controllers/control_estados_controller.rb
@@ -2,6 +2,21 @@ class ControlEstadosController < ApplicationController
 
 def create
   
+  ControlEstado.delete_all()
+  @num=params[:control_estado].size
+  @i=1;
+  
+  begin
+    @ctrlEst=ControlEstado.new(:control_id => params[:control_estado]["control_id#{@i}"], :estado_id => params[:control_estado]["estado_id#{@i}"])
+    @ctrlEst.save
+    @i+=1
+  end until @i>@num/2
+  
+  #params[:control_estado].each do |key, value|
+   # @ctrlEst=ControlEstado.new(:control_id => params[:control_estado].size, :estado_id => value)
+   # @ctrlEst.save
+  #end
+  redirect_to '/auditor/final'
 end
 
 end
